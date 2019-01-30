a680bea1a98765b4dbae79ca02ad794a990ad031
mBarlescu <43458535+mBarlescu@users.noreply.github.com>
2019-01-30 07:00:57 -0800

    clamped player movement to game camera
    player movement is clamped to game camera

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -7,9 +7,25 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+    float xMax;
+    float xMin;
+    float yMax;
+    float yMin;
     // Start is called before the first frame update
     void Start()
     {
+        moveBoundaries();
+    }
+
+    private void moveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+        
 
     }
 
@@ -23,8 +39,8 @@ public class Player : MonoBehaviour
     {
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPosition = transform.position.x + deltaX;
-        var newYPosition = transform.position.y + deltaY;
+        var newXPosition = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPosition = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPosition, newYPosition);
     }
 }

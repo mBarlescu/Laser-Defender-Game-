8c95513a4865332491ddfed2c34a541bf9418c7a
mBarlescu <43458535+mBarlescu@users.noreply.github.com>
2019-01-31 11:07:07 -0800

    added waypoints for enemies

--- LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    [SerializeField] float enemyMoveSpeed = 2f;
+    int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex].transform.position;
+            var movementThisFrame = enemyMoveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+            Debug.Log("MOV111E");
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+                Debug.Log("MOVE");
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 }
